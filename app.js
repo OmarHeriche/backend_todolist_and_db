dbe20c60c77071c7653bf12ae7e5d0e6a398c780
omar_heriche <herricheomar@gamil.com>
2023-11-23 08:47:17 +0100

    after setting the port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static("./public"));
 app.use(notFound);
 app.use(handle_error);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connectDb(process.env.url);
